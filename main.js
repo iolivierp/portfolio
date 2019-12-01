656191364c739c25ca2fe75b7182e8bddfcde46d
Yedenn <ianis.olivier.pacaud@gmail.com>
2019-12-01 18:12:11 +0100

    layout all done, missing content

--- main.js
+++ main.js
@@ -8,26 +8,48 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     window.scrollTo(0,divpos[document.querySelector("div").innerHTML])
 
+
+    // may be useless...
     let body = document.querySelector("body")
 	let wrap = document.querySelector("#wrapper")
 
 	window.addEventListener('wheel', scrollBlock, false)
-    window.addEventListener('wheel',firstScroll)
-
-    let inputs = document.querySelectorAll("input,textarea")
-
-    for (let input of inputs) {
-        input.addEventListener("focus",function() {
-            input.style.borderBottom = "solid 3px orange"
-        })
-        input.addEventListener("focusout",function() {
-            input.style.borderBottom = "solid 3px transparent"
-        })
-    }
-
+    
+    formDesign()
+    loadProjects()
     loadContactLogos()
 
 })
+
+function loadProjects() {
+    let projects = document.querySelectorAll(".project-img")
+    let descs = document.querySelectorAll("#desc div")
+    let img_url = ["bdsm","bdsmw","laphotolab","fnac"]
+
+    for (let index in img_url) {
+        projects[index].style.backgroundImage = "url(images/"+img_url[index]+".png)"
+        
+        projects[index].addEventListener("click", function() {
+            // console.log(descs[index])
+            for (let desc of descs) {
+                desc.style.display = "none"
+            }
+            descs[index].style.display = "block"
+        })
+    }
+}
+
+function formDesign() {
+    let inputs = document.querySelectorAll("input,textarea")
+    for (let input of inputs) {
+        input.addEventListener("focus",function() {
+            input.style.boxShadow = "3px 3px orange"
+        })
+        input.addEventListener("focusout",function() {
+            input.style.boxShadow = "3px 3px transparent"
+        })
+    }
+}
 
 function loadContactLogos() {
     let logo_url = ["github","linkedin","fb"]
@@ -40,16 +62,6 @@ function loadContactLogos() {
             return
         }
     }
-}
-
-function firstScroll() {
-    for (let bar of document.querySelectorAll(".bars")) {
-        setTimeout(function() {
-            bar.style.width = bar.innerHTML * bar.parentNode.offsetWidth / 100 + "px"
-            bar.innerHTML = ""
-        },230)
-    }
-    window.removeEventListener('wheel', firstScroll)
 }
 
 function scrollBlock(event) {
@@ -68,6 +80,29 @@ function scrollBlock(event) {
 	}
 
 	currentdiv.innerHTML = index
+
+    if (index == 2 && document.querySelector(".bars").innerText != "") {
+        for (let bar of document.querySelectorAll(".bars")) {
+            setTimeout(function() {
+                bar.style.width = bar.innerHTML * bar.parentNode.offsetWidth / 100 + "px"
+                bar.innerText = ""
+            },230)
+        }
+    }
+    //-------------OU---------------
+    // if (index == 2) {
+    //     for (let bar of document.querySelectorAll(".bars")) {
+    //         setTimeout(function() {
+    //             bar.style.width = bar.innerHTML * bar.parentNode.offsetWidth / 100 + "px"
+    //             //bar.innerHTML = ""
+    //             bar.style.color = "orange"
+    //         },230)
+    //     }
+    // } else {
+    //      for (let bar of document.querySelectorAll(".bars")) {
+    //             bar.style.width = 0
+    //     }
+    // }
 
 	window.scrollTo(0,divpos[index].offsetTop)
 
